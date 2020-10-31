15076a70023c605e507005a3286e37554fa11b8f
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-11-01 00:45:02 +0530

    Need to add build directory for the publishing purpose

---
+++ build/precache-manifest.6c204a06e81143435191513393da3dda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d61992542d550467c36b3a582131e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d815bd2416e5486d5578",
+    "url": "/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "d71e59d39b0b895ac4ce",
+    "url": "/static/css/main.7b7d3aa6.chunk.css"
+  },
+  {
+    "revision": "d815bd2416e5486d5578",
+    "url": "/static/js/2.46a15ea4.chunk.js"
+  },
+  {
+    "revision": "d71e59d39b0b895ac4ce",
+    "url": "/static/js/main.b767b8ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "432504820baa5d28794781802c514173",
+    "url": "/static/media/employee.43250482.png"
+  }
+]);
